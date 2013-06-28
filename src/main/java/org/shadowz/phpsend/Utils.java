a01ba25101a4109f4ddc97ab09b653e525e04f0b
Paul (Ultimate) <ultimate@burngames.net>
2013-06-28 11:16:51 -0500

    Made logger a bit less spammy

--- src/main/java/org/shadowz/phpsend/Utils.java
+++ src/main/java/org/shadowz/phpsend/Utils.java
@@ -24,25 +24,15 @@ public class Utils {
    public static void log(String msg, LogType type){
       ConsoleCommandSender p = Bukkit.getConsoleSender();
       if(type.equals(LogType.Normal)){
-         p.sendMessage(Utils.color("&8 -=-=-=- &aPHPSend: Message &8-=-=-=-", false));
          p.sendMessage(Utils.color(msg));
-         p.sendMessage(Utils.color("&8 -=-=- &a End PHPSend Message &8-=-=-", false));
       } else if(type.equals(LogType.Bad)){
-         p.sendMessage(Utils.color("&8 -=-=-=- &ePHPSend: Something Bad! &8-=-=-=-", false));
          p.sendMessage(Utils.color("&e" + msg));
-         p.sendMessage(Utils.color("&8 -=-=- &e End PHPSend Message &8-=-=-", false));
       } else if(type.equals(LogType.Broken)){
-         p.sendMessage(Utils.color("&8 -=-=- &cPHPSend: Something Broke! &8-=-=-", false));
          p.sendMessage(Utils.color("&c" + msg));
-         p.sendMessage(Utils.color("&8 -=-=- &c End PHPSend Message &8-=-=-", false));
       } else if(type.equals(LogType.Horrible)){
-         p.sendMessage(Utils.color("&8 -=- &4PHPSend: Something Happened! &8-=-", false));
          p.sendMessage(Utils.color("&4" + msg));
-         p.sendMessage(Utils.color("&8 -=-=- &4 End PHPSend Message &8-=-=-", false));
       } else if(type.equals(LogType.Error)){
-         p.sendMessage(Utils.color("&8 -=-=-=- &4PHPSend: ERROR! &8-=-=-=-", false));
-         p.sendMessage(Utils.color("&e" + msg));
-         p.sendMessage(Utils.color("&8 -=-=- &e End PHPSend ERROR &8-=-=-", false));
+         p.sendMessage(Utils.color("&8[&4PHPSend ERROR!&8]&4" + msg, false));
       }
    }
    
